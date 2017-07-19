54189ef11a878b0bd2faf94b4c4eb7591a4962d1
L706077 <l706077@gmail.com>
2017-07-19 16:50:06 +0800

    first commit

---
+++ build/CMakeFiles/RT_inference_multiModel.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/RT_inference_multiModel.dir/RT_inference_multiModel.cpp.o"
+  "RT_inference_multiModel.pdb"
+  "RT_inference_multiModel"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/RT_inference_multiModel.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
